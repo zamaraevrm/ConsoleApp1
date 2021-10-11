fe911c36f52e90d14fac3a57dcdbd362c2ef792c
bajarka bajarka <zamaraevrm@mer.ci.nsu.ru>
2021-10-11 19:09:05 +0700

    lktjkl

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+
+        delegate int UpTwo(int number);
+
+        delegate void End(int num);
+
+        delegate End Result(Operation op, UpTwo ut);
+        static void Main(string[] args)
+        {
+           
+            Operation del = Add;
+            //int result = del(4, 5); 
+            //Console.WriteLine(result);
+
+            UpTwo upTwo = (int number) => number * 2;
+            //result = upTwo(result);
+            //Console.WriteLine(result);
+
+            Result re = (Operation op, UpTwo ut) =>
+            {
+                End end1 = (int num) => { Console.WriteLine($"result {op(num,ut(num))}"); };
+                return end1;
+            };
+
+            End end = re(del, upTwo);
+            end(3);
+        }
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
